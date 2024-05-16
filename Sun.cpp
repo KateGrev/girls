ce67fdc6497db8e4f63ea27d2977845ae0d1ca32
KateGrev <168267900+KateGrev@users.noreply.github.com>
2024-05-16 15:49:52 +0500

    Update Sun.cpp

--- Sun.cpp
+++ Sun.cpp
@@ -1,24 +1,12 @@
-#include <graphics.h>
-#include <iostream>
-
-int sunX = 80;
-int sunY = 80;
-int sunRadius = 30;
-
-void sun(){
-    setcolor(YELLOW);
-    setfillstyle(SOLID_FILL, YELLOW);
-    fillellipse(sunX, sunY, sunRadius, sunRadius);
-}
-
-int main(){
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
-
-    sun();
-
-    getch();
-    closegraph();
-    
-    return 0;
+#include <graphics.h>  
+#include <iostream>  
+  
+int sunX = 80;  
+int sunY = 80;  
+int sunRadius = 30;  
+  
+void sun(){  
+    setcolor(YELLOW);  
+    setfillstyle(SOLID_FILL, YELLOW);  
+    fillellipse(sunX, sunY, sunRadius, sunRadius);  
 }
