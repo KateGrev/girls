aeee06cf6d0f4dce2120038d59d5e73ff71daaa6
KateGrev <168267900+KateGrev@users.noreply.github.com>
2024-05-23 16:29:01 +0500

    Update fence.cpp

--- fence.cpp
+++ fence.cpp
@@ -1,8 +1,10 @@
 #include <graphics.h>
 #include <picture.hpp>
 void fence(){ 
-    line(50, 300, 50, 500);
-    line(50, 300, 150, 300);
-    line(150, 300, 150, 500);
-    line(50, 500, 150, 500);
+     setcolor(WHITE);
+    line(0, 350, 100, 350);
+    line(0, 450, 100, 450);
+    line(30, 350, 30, 450);
+    line(60, 350, 60, 450);
+       line(90, 350, 90, 450);
 }
